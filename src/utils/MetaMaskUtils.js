8289b88eebde91ea749e296f09872029506626d9
sethkfman <seth.kaufman@consensys.net>
2021-06-09 11:26:22 -0600

    added get public address method

---
+++ src/utils/MetaMaskUtils.js
@@ -0,0 +1,9 @@
+
+/**
+ * Returns promise fetching current connected ethereum address
+ */
+export default async function  getConnectedPublicAddress () {
+    const { ethereum } = window;
+    return await ethereum
+    .request({method: 'eth_accounts'})
+}
