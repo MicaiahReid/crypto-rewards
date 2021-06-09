bbf03676434fdf8c44a705da13873c3a08a6724d
sethkfman <seth.kaufman@consensys.net>
2021-06-08 20:11:01 -0600

    refactored navigation and added menu to display other pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,53 +1,12 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Campaign from '../Campaign/Campaign';
-import OnboardingButton from './OnboardingButton/OnboardingButton';
+import NavigationMenu from './NavigationMenu/NavigationMenu';
 
 function App() {
-  
-  const classes = useStyles();
-
   return (
     <>
-        <AppBar position="static">
-          <Toolbar>
-
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-
-            <Typography variant="h6" className={classes.title}>
-              Crypto Rewards
-            </Typography>
-
-            <OnboardingButton color= "inherit">Connect Wallet</OnboardingButton>
-          
-          </Toolbar>
-
-        </AppBar>
-
-        <div>
-          <Campaign> </Campaign>
-        </div>
+      <NavigationMenu></NavigationMenu>
     </>
   );
 }
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  }
-}));
 
 export default App;
