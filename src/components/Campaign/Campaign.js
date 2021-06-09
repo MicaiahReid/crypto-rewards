e0ecc2d02229b8922873779527a13d9bc5a22e14
MicaiahReid <micaiahreid@gmail.com>
2021-06-09 10:37:00 -0400

    Cleaning up unused libraries

--- src/components/Campaign/Campaign.js
+++ src/components/Campaign/Campaign.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import CardActions from "@material-ui/core/CardActions";
 import Collapse from "@material-ui/core/Collapse";
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from "@material-ui/core/IconButton";
-import ButtonBase from '@material-ui/core/ButtonBase';
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import ReactMarkdown from 'react-markdown';
 
