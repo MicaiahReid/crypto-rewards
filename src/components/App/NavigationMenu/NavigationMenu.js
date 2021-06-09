f03c71cb05206c21bcae5368239c4e6dc97238e6
MicaiahReid <micaiahreid@gmail.com>
2021-06-09 09:25:59 -0400

    Format campaign cards some. Render cards dynamically

--- src/components/App/NavigationMenu/NavigationMenu.js
+++ src/components/App/NavigationMenu/NavigationMenu.js
@@ -17,34 +17,58 @@ function NavigationMenu () {
   
     const handleChange = (event, newValue) => {
       setValue(newValue);
-    };
+    };  
+    const data = [
+      {
+        id: "1",
+        title: "Uniswap Trade",
+        shortDescription:
+          "Trade 10 Uniswap tokens for the first time to earn UNI rewards!",
+        subtitle: "",
+        reward: "5 UNI",
+        longDescription: "Test",
+      },
+
+      {
+        id: "2",
+        title: "Compound Investments",
+        shortDescription:
+          "Put 50 COMP in a compound liquidity pool for 3 months and earn COMP rewards!",
+        subtitle: "",
+        reward: "50 COMP",
+        longDescription: "Test",
+      },
+    ];
+    const campaigns = data.map((campaign) => {
+      return <Campaign campaign={campaign}></Campaign>;
+    });
 
     return (
-        <>
-            <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6" className={classes.title}>
-                    Crypo Rewards
-                </Typography>
-                <OnboardingButton color= "inherit">Connect Wallet</OnboardingButton>
-            </Toolbar>
-            <Tabs value={value} onChange={handleChange}>
-                <Tab label="Campaigns" {...a11yProps(0)} />
-                <Tab label="Resume" {...a11yProps(1)} />
-                <Tab label="Create Campaign" {...a11yProps(2)} />
-            </Tabs>
-            </AppBar>
+      <>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Crypo Rewards
+            </Typography>
+            <OnboardingButton color="inherit">Connect Wallet</OnboardingButton>
+          </Toolbar>
+          <Tabs value={value} onChange={handleChange}>
+            <Tab label="Campaigns" {...a11yProps(0)} />
+            <Tab label="Resume" {...a11yProps(1)} />
+            <Tab label="Create Campaign" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
 
-            <TabPanel value={value} index={0}>
-                <Campaign> </Campaign>
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                Resume Page
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                Create Campaign Page
-            </TabPanel>
-        </>
+        <TabPanel value={value} index={0}>
+          <div className="body">{campaigns}</div>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          Resume Page
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Create Campaign Page
+        </TabPanel>
+      </>
     );
 
 }
--- src/components/Campaign/Campaign.js
+++ src/components/Campaign/Campaign.js
@@ -1,68 +1,105 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardHeader from "@material-ui/core/CardHeader";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography';
+import IconButton from "@material-ui/core/IconButton";
 import ButtonBase from '@material-ui/core/ButtonBase';
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
-  paper: {
+  card: {
     padding: theme.spacing(2),
-    margin: 'auto',
-    maxWidth: 500,
+    margin: "100px",
+    maxWidth: "60%"
   },
   image: {
     width: 128,
     height: 128,
   },
   img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+  avatar: {
+    backgroundColor: "#ff84f6",
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
   },
 }));
 
-export default function Campaign() {
+export default function Compaign({campaign}) {
+  console.log(campaign);
   const classes = useStyles();
-
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase className={classes.image}>
-              Pending
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                  Uniswap Campaign
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                  Execute a trade
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                  ID: 1030114
-                </Typography>
-              </Grid>
-              <Grid item>
-                <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                  Enroll
-                </Typography>
-              </Grid>
+      <Card className={classes.card}>
+        <CardContent>
+          <Grid container>
+            <Grid item xs={2}>
+              <Typography>
+                {
+                  <Avatar className={classes.avatar}>
+                    {campaign.title.substring(0, 1)}
+                  </Avatar>
+                }
+              </Typography>
             </Grid>
-            <Grid item>
-              <Typography variant="subtitle1">Reward 1.00 eth</Typography>
+            <Grid item xs={5}>
+              <Typography variant="h4" component="h2">
+                {campaign.title}
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h4" component="h2">
+                {campaign.reward}
+              </Typography>
             </Grid>
           </Grid>
-        </Grid>
-      </Paper>
+          <Typography variant="body2" colot="textSecondary" component="p">
+            {campaign.shortDescription}
+          </Typography>
+        </CardContent>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>{campaign.longDescription}</Typography>
+          </CardContent>
+        </Collapse>
+        <CardActions>
+          Enroll
+          <IconButton
+            className={classes.expand + classes.expandOpen ? + " " + expanded : ""}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
     </div>
   );
 }
