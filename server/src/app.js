749f2e678900c84ccc8d9f72b090ab27e6e22391
MicaiahReid <micaiahreid@gmail.com>
2021-06-10 11:07:51 -0400

    Use cors library

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
 const app = express();
 
-const mongodbUri = process.env.MONGODB_URI || "mongodb://localhost/crypto-rewards";
+const mongodbUri =
+  process.env.MONGODB_URI || "mongodb://localhost/crypto-rewards";
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
@@ -13,14 +15,7 @@ const options = {
 mongoose.connect(mongodbUri, options);
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+app.use(cors());
 app.get("/", (req, res) => res.send("Welcome to Crypto Rewards"));
 app.use("/api/", routes.campaign);
 
