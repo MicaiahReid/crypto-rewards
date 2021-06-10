d6a5903733a582f6ff0fc8115f2e1813d0d5ac2d
MicaiahReid <micaiahreid@gmail.com>
2021-06-10 09:03:15 -0400

    allow requests from localhost on server

--- server/src/app.js
+++ server/src/app.js
@@ -13,6 +13,14 @@ const options = {
 mongoose.connect(mongodbUri, options);
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.get("/", (req, res) => res.send("Welcome to Crypto Rewards"));
 app.use("/api/", routes.campaign);
 
