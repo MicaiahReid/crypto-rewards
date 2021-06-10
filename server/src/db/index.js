68afcfd8c3168dbda37d84f8431405b48dbd2296
PeterYinusa <peter.yinusa@consensys.net>
2021-06-10 20:25:33 +0100

    merge main

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const mongodbUri = process.env.MONGODB_URI || "mongodb://localhost/crypto-rewards";
 const options = {
+  useCreateIndex: true,
   useUnifiedTopology: true,
   useNewUrlParser: true,
 };
