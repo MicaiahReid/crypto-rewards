7eca8f35952fd4f79be26e95e6f2e7ee94bdc472
liang Liang <lilysway@hotmail.com>
2021-06-10 19:04:51 -0400

    cleaned up format and started working on Verify.sol

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,90 +1,74 @@
 pragma solidity ^0.8.0;
-import "../github/OpenZeppelin/openzeppelin-contracts/contracts/utils/escrow/Escrow.sol";
-//import "../github/OpenZeppelin/open-zeppelin/contracts/Ownable.sol";
-contract Campaigns is Escrow {
 
-            event Deposited(int256 indexed campaignID, uint256 payoutAmount);
-            event Withdrawn(address indexed claim, uint256 payoutAmount);
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-            //claimee struct claimees wallet address, transaction hash, yes or no (bool)
-            //creating a mapping of claimees, users in this mapping are already verfiied
-            //verify contract gets the result from verify function and adds the result to the claim struct
-            //if the bool is yes for the claim, call the payout()
+contract Campaigns is Ownable {
+    event Deposited(uint256 indexed campaignID, uint256 amount);
+    event Withdrawn(address indexed claim, uint256 payoutAmount);
 
-            uint rewardunit = 0; ///in this case the reward amount will be in WEI
-            uint startdate = 0;
-            uint enddate = 0;
-            
-            constructor(uint _rewardunit, uint _startdate, uint _endate){
-                owner = msg.sender;
-                _rewardunit = rewardunit;
-                _startdate = startdate;
-                _endate = endate;
+    uint256 rewardunit = 0; ///in this case the reward amount will be in WEI? amount of tokens to award out
+    //how would this rewardunit work with different tokens?
+    uint256 startdate = 0;
+    uint256 enddate = 0;
 
+    constructor(
+        uint256 _rewardunit,
+        uint256 _startdate,
+        uint256 _endate
+    ) {
+        owner = msg.sender;
+        _rewardunit = rewardunit;
+        _startdate = startdate;
+        _endate = endate;
+    }
 
-                
-            }
-                struct claimer
-                    {
-                        // Declaring different
-                        // structure elements
-                        address payable claimee;
-                        uint256 transaction;
-                        bool claimed;
+    //claimee struct claimees wallet address, transaction hash, yes or no (bool)
+    //creating a mapping of claimees, users in this mapping are already verfiied
 
-                        
+    struct claimer {
+        address payable claimee;
+        uint256 txn;
+        //whether or not the claimer has previously claimed
+        bool claimed;
+    }
 
-                    }
-                    
-                mapping (claimer => address) public claimees;
+    mapping(address => claimer) public claimees;
+    address[] public claimerRecord;
 
-                //campapign struct campaign ID, contract address of the campapign, amount of payout
+    //campapign struct campaign ID, contract address of the campapign, amount of payout
 
-                // struct campaign {
-                //     int256 campaignId;
-                //     address campapignContract;
-                //     int256 payoutAmount;
-                // }
+    function deposit(uint256 _amount) private {
+        //should this be two functions or one
+        address(this).balance += _amount;
+        //create mappings for campapign that has campapign id and campapign contract addresses
+        //deposit function that creates a new contract instance for the protocol
+        //deposit function should also set msg.sender as owner of new contract
+        emit Deposited(_amount);
+    }
 
-                deposit(int256 _payoutAmount) {
-                        //should this be two functions or one
-                     address(this).balance += _payoutAmount;                   
-                        //create mappings for campapign that has campapign id and campapign contract addresses
-                        //deposit function that creates a new contract instance for the protocol
-                        //deposit function should also set msg.sender as owner of new contract
-                     emit Deposited( payoutAmount);
-
-                }
-
-                 function getClaimee() public view returns (address _address, uint256 _transaction, bool _claimed ) {
-                     Claimee memory c = claims[id]
-                    return (c.claimee, c.transaction, c.claimed);  //may not need this entire function need to verify if necessary
-                    }
-
-
-                function payout(string _verification, uint256 _transaction) public {
-                        // Check enough balance available, otherwise just return balance
-                        if (_verification = string yes && Claimer.claimed = false) {
-                        claimees[msg.sender].Claimer.claimed = true;
-                        claimees[msg.sender].Claimer.claimee = msg.sender;
-                        claimees[msg.sender].Claimer.transaction = _transaction; 
-                        address(this).balance -= rewardunit;
-                        msg.sender.transfer(rewardunit);
-                        //emit reward claimed event here
-                        emit Withdrawn(uint rewardunit address msg.sender)
-                    }
-
-
-                        endCampaign() onlyOwner {
-                            //end campaign and withdraw funds if they are funds left after the duration of campaign ended.
-                           if(address(this).balance > 0 && now => enddate){
-                               msg.sender.transfer(balance); ///check the synatx 
-                           }
-                           //end campaign if the funds ran out before the campaign ends.
-                           if(addres(this).balance){
-                               enddate = now;
-                           }    
-                        }
+    function payout(string _verification, uint256 _transaction) public {
+        // Check enough balance available, otherwise just return balance
+        if (_verification == true && Claimer.claimed == false) {
+            claimees[msg.sender].Claimer.claimed = true;
+            claimees[msg.sender].Claimer.claimee = msg.sender;
+            claimees[msg.sender].Claimer.transaction = _transaction;
+            address(this).balance -= rewardunit;
+            msg.sender.transfer(rewardunit);
+            //emit reward claimed event here
+            emit Withdrawn(msg.sender, rewardunit);
+        } else {
+            //handle cases that doesn't meet the payout criterias
         }
+    }
 
-
+    function endCampaign() public onlyOwner {
+        //end campaign and withdraw funds if they are funds left after the duration of campaign ended.
+        if (address(this).balance > 0 && now >= enddate) {
+            msg.sender.transfer(balance); ///check the synatx
+        }
+        //end campaign if the funds ran out before the campaign ends.
+        if (addres(this).balance) {
+            enddate = now;
+        }
+    }
+}
---
+++ contracts/Verify.sol
@@ -0,0 +1,4 @@
+pragma solidity ^0.8.0;
+//gets returned result from verification function
+//setClaimer in the Campaign contract
+//run the payout function in campaign contract
