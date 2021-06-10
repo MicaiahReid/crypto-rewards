d4ed3f94336c9aed45bd7f53e336dcc2a4772be2
MicaiahReid <micaiahreid@gmail.com>
2021-06-10 19:47:37 -0400

    add user-based status to campaigns server side

--- server/src/routes/campaign.js
+++ server/src/routes/campaign.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controllers = require("../controllers");
 
-router.route("/campaigns").get(controllers.campaign.index);
+router.route("/campaigns/:address?").get(controllers.campaign.index);
 
 module.exports = router;
