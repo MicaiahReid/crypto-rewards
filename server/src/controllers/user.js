f223b815da911ddc47d82dddeedd8c2956e5b07c
MicaiahReid <micaiahreid@gmail.com>
2021-06-10 19:47:02 -0400

    make calls to create user once account is connected

--- server/src/controllers/user.js
+++ server/src/controllers/user.js
@@ -4,22 +4,18 @@ exports.new = async (req, res) => {
   const { address } = req.body;
   let cryptoRewardsUser;
   try {
-    cryptoRewardsUser = new user({address: address});
+    cryptoRewardsUser = new user({ address: address });
     await cryptoRewardsUser.save();
-  } catch(error) {
+  } catch (error) {
     return res.status(400).json({
       error,
     });
   }
-  return res.status(200).json({
-    data: cryptoRewardsUser,
-  });
+  return res.status(200).json(cryptoRewardsUser);
 };
 
 exports.view = async (req, res) => {
   const { address } = req.params;
-  const cryptoRewardsUser = await user.findOne({address: address});
-  return res.status(200).json({
-    data: cryptoRewardsUser || {},
-  });
+  const cryptoRewardsUser = await user.findOne({ address: address });
+  return res.status(200).json(cryptoRewardsUser || {});
 };
--- src/components/App/NavigationMenu/OnboardingButton/OnboardingButton.js
+++ src/components/App/NavigationMenu/OnboardingButton/OnboardingButton.js
@@ -1,15 +1,17 @@
-import MetaMaskOnboarding from '@metamask/onboarding';
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import MetaMaskOnboarding from "@metamask/onboarding";
+import React from "react";
+import Button from "@material-ui/core/Button";
+import axios from "../../../../utils/API";
 
-const ONBOARD_TEXT = 'Click here to install MetaMask!';
-const CONNECT_TEXT = 'Connect';
-const CONNECTED_TEXT = 'Connected';
+const ONBOARD_TEXT = "Click here to install MetaMask!";
+const CONNECT_TEXT = "Connect";
+const CONNECTED_TEXT = "Connected";
 
 export default function OnboardingButton() {
   const [buttonText, setButtonText] = React.useState(ONBOARD_TEXT);
   const [isDisabled, setDisabled] = React.useState(false);
   const [accounts, setAccounts] = React.useState([]);
+  const [userId, setUserId] = React.useState("");
   const onboarding = React.useRef();
 
   React.useEffect(() => {
@@ -21,6 +23,32 @@ export default function OnboardingButton() {
   React.useEffect(() => {
     if (MetaMaskOnboarding.isMetaMaskInstalled()) {
       if (accounts.length > 0) {
+        const account = accounts[0];
+        if (userId === "") {
+          axios
+            .get(`/api/user/${account}`)
+            .then(({ data }) => {
+              if (data && data._id) {
+                setUserId(data._id);
+              } else {
+                axios
+                  .post("/api/user/", { address: account })
+                  .then(({ data }) => {
+                    if (data && data._id) {
+                      setUserId(data._id);
+                    } else {
+                      console.log("error creating user");
+                    }
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+              }
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
         setButtonText(CONNECTED_TEXT);
         setDisabled(true);
         onboarding.current.stopOnboarding();
@@ -29,7 +57,7 @@ export default function OnboardingButton() {
         setDisabled(false);
       }
     }
-  }, [accounts]);
+  }, [accounts, userId]);
 
   React.useEffect(() => {
     function handleNewAccounts(newAccounts) {
@@ -37,11 +65,11 @@ export default function OnboardingButton() {
     }
     if (MetaMaskOnboarding.isMetaMaskInstalled()) {
       window.ethereum
-        .request({ method: 'eth_requestAccounts' })
+        .request({ method: "eth_requestAccounts" })
         .then(handleNewAccounts);
-      window.ethereum.on('accountsChanged', handleNewAccounts);
+      window.ethereum.on("accountsChanged", handleNewAccounts);
       return () => {
-        window.ethereum.off('accountsChanged', handleNewAccounts);
+        window.ethereum.off("accountsChanged", handleNewAccounts);
       };
     }
   }, []);
@@ -49,7 +77,7 @@ export default function OnboardingButton() {
   const onClick = () => {
     if (MetaMaskOnboarding.isMetaMaskInstalled()) {
       window.ethereum
-        .request({ method: 'eth_requestAccounts' })
+        .request({ method: "eth_requestAccounts" })
         .then((newAccounts) => setAccounts(newAccounts));
     } else {
       onboarding.current.startOnboarding();
