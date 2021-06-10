db00762c5def9740a67d31517ca8a70243505924
sethkfman <seth.kaufman@consensys.net>
2021-06-10 09:34:39 -0600

    added achievements table view

---
+++ src/components/App/Achievements/Achievements.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+class Achievements extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          campaigns: props.campaigns
+        }
+    }
+
+    useStyles = makeStyles({
+        table: {
+          minWidth: 650,
+        },
+      });
+      
+
+    render() {
+        const classes = this.useStyles;
+        
+        return (
+            <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Protocol</TableCell>
+                    <TableCell align="right">Challenge</TableCell>
+                    <TableCell align="right">Reward</TableCell>
+                    <TableCell align="right">Status</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {this.state.campaigns.map((row) => (
+                    <TableRow key={row.title}>
+                    <TableCell component="th" scope="row">
+                        {row.title}
+                    </TableCell>
+                    <TableCell align="right">{row.shortDescription}</TableCell>
+                    <TableCell align="right">{row.reward}</TableCell>
+                    <TableCell align="right">{row.id}</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+        );
+    }
+}
+
+export default Achievements;
