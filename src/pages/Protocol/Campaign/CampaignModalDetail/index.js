0216964102cd324382ebca3a4ee89211382af00f
sethkfman <seth.kaufman@consensys.net>
2021-06-10 13:43:52 -0600

    updated naming

---
+++ src/pages/Protocol/Campaign/CampaignModalDetail/index.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
+import ReactMarkdown from 'react-markdown';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import CloseIcon from '@material-ui/icons/Close';
+import Link from '@material-ui/core/Link';
+import RoundButton from "../../../components/round-button";
+
+class CampaignModalDetail extends React.Component {
+
+    useStyles() {
+        return makeStyles((theme) => ({
+            root: {
+                margin: 0,
+                padding: theme.spacing(2),
+              },
+              closeButton: {
+                position: 'absolute',
+                right: theme.spacing(1),
+                top: theme.spacing(1),
+                color: theme.palette.grey[500],
+              },
+            }));
+      }
+
+    render() {
+        const classes = this.useStyles();
+        return(
+            <Dialog onClose={this.props.onClose} aria-labelledby="challenge-dialog-title" open={this.props.open}>
+                    <MuiDialogTitle disableTypography dividers className={classes.root} id="customized-dialog-title">
+                        <Grid justify="space-between" container spacing={24} >
+                            <Typography variant="h6">{this.props.modalTitle}</Typography>
+
+                            {this.props.onClose ? (
+                                <IconButton aria-label="close" className={classes.closeButton} onClick={this.props.onClose}>
+                                <CloseIcon />
+                                </IconButton>) : null}
+                        </Grid>
+                    </MuiDialogTitle>
+                <DialogContent dividers >
+                    <Typography gutterBottom>
+                        To learn more about this protocol, visit: <Link href="https://uniswap.org/about/">About Uniswap</Link>
+                    </Typography>
+
+                    <Typography gutterBottom>
+                        <ReactMarkdown>{this.props.modalDetails}</ReactMarkdown>
+                    </Typography>
+                </DialogContent>
+                <DialogContent>
+                    <RoundButton
+                    onClick={(e) => this.props.callToAction}
+                    label={this.props.callToActionState}
+                    />
+                </DialogContent>    
+            </Dialog>
+        );
+    }
+}
+
+export default CampaignModalDetail;
---
+++ src/pages/Protocol/Campaign/index.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from "@material-ui/core/CardActions";
+import Typography from '@material-ui/core/Typography';
+import RoundButton from "../../components/round-button";
+import Box from '@material-ui/core/Box';
+import CampaignModalDetail from './CampaignModalDetail';
+import ButtonBase from  '@material-ui/core/ButtonBase';
+import Grid from '@material-ui/core/Grid';
+
+class Campaign extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      enrolled: props.campaign.userEnrolled,
+      displayModal: null
+    }
+  }
+
+  useStyles() {
+    return makeStyles((theme) => ({
+      root: {
+        flexWrap: "wrap",
+        flexGrow: 1
+      },
+      card: {
+        padding: theme.spacing(2),
+        margin: "100px",
+        width: "60%",
+      },
+      image: {
+        width: 128,
+        height: 128,
+      },
+      img: {
+        margin: "auto",
+        display: "block",
+        maxWidth: "100%",
+        maxHeight: "100%",
+      },
+    }));
+  }
+
+  enroll(id) {
+    // console.log(id);
+  }
+
+  handleClick = (item) => {
+    this.setState({
+      displayModal: true
+    })
+  };
+
+  handleClose = () => {
+    this.setState({
+      displayModal: null
+    })  
+  };
+
+  render() {
+    const open = Boolean(this.state.displayModal);
+    const campaign = this.props.campaign;
+    const classes = this.useStyles();
+    return (
+      <Box className={classes.root}>
+        <Card className={classes.card}>
+          <ButtonBase id="card_button" onClick={(e) => {this.handleClick(e)}}>
+            <Grid container spacing={8} align='left' justify='flex-end' direction='column' alignItems="stretch">
+              <Grid item sx={2}>
+                <Typography variant="h4" component="h2">
+                  {campaign.title}
+                </Typography>
+              </Grid>
+              <Grid item sx={2}>
+                <Typography variant="body1" component="p">
+                  {campaign.shortDescription}
+                </Typography>
+              </Grid>
+              <Grid item sx={2}>
+                <Typography variant="body1">
+                  Reward: <b>{campaign.reward}</b>
+                </Typography>
+              </Grid>
+          </Grid>
+          </ButtonBase>
+          <CardActions>
+                <RoundButton
+                  onClick={(e) => this.enroll(campaign.id)}
+                  label={this.state.enrolled ? "Claim" : "Enroll"}
+                  id="enroll_button"
+                />
+              </CardActions>
+        </Card>
+        <CampaignModalDetail 
+          open={open} 
+          onClose={this.handleClose} 
+          modalTitle={campaign.title}
+          modalDetails={campaign.longDescription}
+          callToAction={this.enroll(campaign.id)}
+          callToActionState={this.state.enrolled ? "Claim" : "Enroll"}
+        >
+        </CampaignModalDetail>
+      </Box>
+    );
+  }
+}
+export default Campaign;
