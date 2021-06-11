432ae682d8611e271cc85083d6a36975769df2de
Cal-L <cleun007@gmail.com>
2021-06-11 14:07:56 -0700

    Clean up campaigns fetch saga

--- src/services/saga.js
+++ src/services/saga.js
@@ -1,20 +1,18 @@
-import { all, takeLatest, put, call, delay } from "redux-saga/effects";
+import { all, takeLatest, put, call } from "redux-saga/effects";
 import { ActionTypes } from "./redux/index";
 import axios from "../utils/API";
 import getConnectedPublicAddress from "../utils/MetaMaskUtils";
 import { setCampaigns } from "./redux/actions";
 
 function* fetchCampaignsApi() {
-  console.log("Hello");
   try {
     const accounts = yield call(getConnectedPublicAddress);
     let getPath = "/api/campaigns";
     if (accounts.length > 0) {
       getPath += "/" + accounts[0];
     }
-    let campaigns = yield call(axios.get, getPath);
-    yield put(setCampaigns(campaigns));
-    console.log("GOT CAMPAIGNS", campaigns);
+    let results = yield call(axios.get, getPath);
+    yield put(setCampaigns(results.data));
   } catch (error) {
     console.log("Error fetching campaigns:", error);
   }
