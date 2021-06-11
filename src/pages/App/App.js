e8f5361244747c7a6b48da8cd3bfbbe8df191754
sethkfman <seth.kaufman@consensys.net>
2021-06-11 14:48:30 -0600

    added footer

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,10 +1,48 @@
 import React from "react";
 import NavigationMenu from "./NavigationMenu/NavigationMenu";
+import Link from "@material-ui/core/Link";
 
 function App() {
   return (
     <>
-      <NavigationMenu></NavigationMenu>
+      <div
+        stytle={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <NavigationMenu></NavigationMenu>
+        <div
+          style={{
+            display: "flex",
+            marginTop: 8,
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <div
+            style={{
+              fontSize: 13,
+              marginRight: 4,
+            }}
+          >
+            Have a suggestion for another chanllenge?{" "}
+            <u>
+              <Link
+                style={{
+                  color: "black",
+                  fontSize: 13,
+                  fontWeight: "800",
+                  underline: true,
+                }}
+                href="https://github.com/"
+              >
+                Submit one here.
+              </Link>
+            </u>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
