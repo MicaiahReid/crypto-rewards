81843cc508833a62d3434a3877949d7584b3fef0
MicaiahReid <micaiahreid@gmail.com>
2021-06-10 21:27:19 -0400

    add verify route

--- server/src/controllers/userCampaign.js
+++ server/src/controllers/userCampaign.js
@@ -1,5 +1,6 @@
 const { userCampaign, user } = require("../models");
-const { getLatestBlockNum } = require("../verification/web3-helper");
+const { getLatestBlockNum } = require("../utils/web3-helper");
+const { verify } = require("../utils/verify");
 
 exports.enroll = async (req, res) => {
   const { address, campaignId } = req.body;
@@ -27,6 +28,68 @@ exports.enroll = async (req, res) => {
     } catch (error) {
       return res.status(400).json({
         error,
+      });
+    }
+  } else {
+    return res.status(400).json({
+      error: "user not found",
+    });
+  }
+};
+
+exports.verify = async (req, res) => {
+  const { address, campaignId } = req.body;
+  const cryptoRewardsUser = await user.findOne({ address: address });
+
+  if (cryptoRewardsUser !== undefined) {
+    const userId = cryptoRewardsUser._id;
+    // we need the enrollment block data, so get the userCampaign
+    const userCampaignInst = await userCampaign.findOne({
+      user: userId,
+      campaign: campaignId,
+    });
+    if (userCampaignInst !== undefined) {
+      try {
+        // save the latest block so we have a snapshot of when they clicked verify
+        const verifyBlock = await getLatestBlockNum();
+        userCampaignInst.verifyBlock = verifyBlock;
+
+        await userCampaignInst.save();
+        // now get the rest of the data we'll need for verification
+        await userCampaignInst
+          .populate("user")
+          .populate("campaign")
+          .execPopulate();
+        const verificationData = {
+          userAddress: userCampaignInst.user.address,
+          campaignAddress: userCampaignInst.campaign.address,
+          verificationType: userCampaignInst.campaign.verificationType,
+          enrollBlock: userCampaignInst.enrollBlock,
+          verifyBlock: verifyBlock,
+        };
+        // and not let's verify!
+        const success = await verify(verificationData);
+        console.log(success);
+        if (success === true) {
+          userCampaignInst.claimedReward = true;
+          userCampaignInst.save();
+          return res.status(200).json({
+            success: success,
+          });
+        } else {
+          return res.status(400).json({
+            error:
+              "failed to verify " + userCampaignInst.campaign.verificationType,
+          });
+        }
+      } catch (error) {
+        return res.status(400).json({
+          error,
+        });
+      }
+    } else {
+      return res.status(400).json({
+        error: "user not enrolled in campaign",
       });
     }
   } else {
