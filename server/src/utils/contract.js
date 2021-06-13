2b50c15da25afaa84bd257ebf092f8013743065a
MicaiahReid <micaiahreid@gmail.com>
2021-06-13 00:15:50 -0400

    change function calls/deployment to match our contract

--- server/src/utils/contract.js
+++ server/src/utils/contract.js
@@ -1,47 +1,38 @@
 const { ethers, Contract } = require("ethers");
-const { piggybankAbi, piggybankBytecode } = require("../../contract/constants.json");
+const {
+  contractAbi,
+  contractBytecode,
+} = require("../../contract/constants.json");
 
 const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
 const signer = provider.getSigner();
 
-exports.deploy = async () => {
-  const piggybank = new ethers.ContractFactory(
-    piggybankAbi,
-    piggybankBytecode,
-    signer,
-  )
-  let contract = await piggybank.deploy();
+// deploys and funds a contract by a specified amount until endDate is reached
+exports.deploy = async (endDate, amountToFund) => {
+  const campaignContract = new ethers.ContractFactory(
+    contractAbi,
+    contractBytecode,
+    signer
+  );
+  let contract = await campaignContract.deploy(endDate, {
+    from: process.env.INITIAL_FUND_ADDRESS,
+    value: amountToFund,
+  });
   contract = await contract.deployTransaction.wait();
   process.env.CONTRACT_ADDRESS = contract.contractAddress;
   return contract;
 };
 
-const getPiggyBank = async () => {
+const getCampaignContract = async () => {
   const contractAddress = process.env.CONTRACT_ADDRESS;
-  const piggybank = new Contract(
-    contractAddress,
-    piggybankAbi,
-    signer,
-  )
-  return piggybank;
+  const contract = new Contract(contractAddress, contractAbi, signer);
+  return contract;
 };
 
-exports.deposit = async () => {
-  const piggybank = await getPiggyBank();
-  const fromAddress = await piggybank.owner();
-  const transaction = await piggybank.deposit({
-    from: fromAddress,
-    value: ethers.utils.parseEther("5")
-  });
-  return transaction;
-};
-
-exports.withdraw = async () => {
-  const piggybank = await getPiggyBank();
-  const fromAddress = await piggybank.owner();
-  const transaction = await piggybank.withdraw(
-    ethers.utils.parseEther("1"), { 
-      from: fromAddress
+exports.payout = async (amount, address) => {
+  const contract = await getCampaignContract();
+  const transaction = await contract.payout(true, address, amount, {
+    from: process.env.CONTRACT_ADDRESS,
   });
   return transaction;
 };
