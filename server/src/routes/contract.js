1d7c13dfacb5857f58ec3a877dd69b62af51a0d1
PeterYinusa <peter.yinusa@consensys.net>
2021-06-14 13:54:54 +0100

    remove unused routes

--- server/src/routes/contract.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controllers = require("../controllers");
-
-router.route("/contract/deploy").get(controllers.contract.deploy);
-router.route("/contract/deposit").get(controllers.contract.deposit);
-router.route("/contract/withdraw").get(controllers.contract.withdraw);
-
-module.exports = router;
